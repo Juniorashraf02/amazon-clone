f272b7c72e4909d1c3968ded6435eb5736b9b475
juniorashraf02 <shuvo.istiak11@gmail.com>
2023-03-27 03:00:14 +0600

    firebase config added

---
+++ firebase.config.tsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDiA2pqVJM8U1PwbtPfVf4HY8G5lYoUVrI",
+    authDomain: "clone-19e24.firebaseapp.com",
+    projectId: "clone-19e24",
+    storageBucket: "clone-19e24.appspot.com",
+    messagingSenderId: "508016523822",
+    appId: "1:508016523822:web:04185e42000a350d45bdbf"
+  };
